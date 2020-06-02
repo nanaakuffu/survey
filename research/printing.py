cc91a93d0c53324c8b693de06faeb8caa7073376
nanaakuffu <nakuffu@aims.ac.tz>
2020-06-02 02:23:55 +0000

    Added pdf creation for the system

--- research/printing.py
+++ research/printing.py
@@ -1,11 +1,11 @@
 from survey.settings import STATICFILES_DIRS
-import os
 
 from reportlab.lib.pagesizes import letter, A4
 from reportlab.platypus import SimpleDocTemplate, Paragraph, HRFlowable, Spacer, Image
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.lib.enums import TA_CENTER
-from reportlab.lib.units import inch
+from reportlab.lib.enums import TA_CENTER, TA_RIGHT
+from reportlab.lib.units import inch, mm
+from reportlab.lib import colors
 
 class PDF():
     def __init__(self, buffer, pagesize):
@@ -22,47 +22,79 @@ class PDF():
         canvas.saveState()
         styles = getSampleStyleSheet()
 
+        customColor = colors.Color(red=(46/255),green=(117/255),blue=(181/255))
+
+        style_right = ParagraphStyle(name='right',
+                                     alignment=TA_RIGHT,
+                                     textColor=customColor)
+        style_center = ParagraphStyle(name='center',
+                                     alignment=TA_CENTER,
+                                     textColor=colors.black)
+        
         # Header
-        header = Paragraph('This is a multi-line header. It goes on every page. ' * 5, styles['Normal'])
-        w, h = header.wrap(doc.width, doc.topMargin)
-        header.drawOn(canvas, doc.leftMargin, doc.height + doc.topMargin - h)
+        IMAGE_PATH = STATICFILES_DIRS[0] + "/survey/img/new_survey_logo.jpg"
+        imageHeader = Image(IMAGE_PATH, 4.1 * inch, 1.2 * inch)
+        imageHeader.hAlign = 'LEFT'
+        headerText = Paragraph('<font size="20"><b>MyBQualityScan</b></font>', style_right)
+        subheaderText = Paragraph('<font size="15"><b>QI Recommendation</b></font>', style_right)
+
+        # This is needed for the drawOn for Paragraph to work
+        w, h = headerText.wrap(doc.width, doc.topMargin)
+        w1, h1 = subheaderText.wrap(doc.width, doc.topMargin)
+        # Draw image and Text on the pdf canvas
+        imageHeader.drawOn(canvas, doc.leftMargin, doc.height + doc.topMargin - 30)
+        headerText.drawOn(canvas, doc.leftMargin - 10, doc.height + doc.topMargin + 30)
+        subheaderText.drawOn(canvas, doc.leftMargin - 10, doc.height + doc.topMargin)
 
         # Footer
-        footer = Paragraph('This is a multi-line header. It goes on every page. ' * 5, styles['Normal'])
+        page_number_text = "%d" % (doc.page)
+        footer = Paragraph(page_number_text, style_center)
         w, h = footer.wrap(doc.width, doc.bottomMargin)
-        footer.drawOn(canvas, doc.leftMargin, h)
+        footer.drawOn(canvas, doc.leftMargin, h + 20)
 
         canvas.restoreState()
 
 
-    def makePDF(self, data):
+    def makePDF(self, hData, data):
         try:
             buffer = self.buffer
-            doc = SimpleDocTemplate(buffer, 
-                                    rightMargin=inch/4,
-                                    leftMargin=inch/4, 
-                                    topMargin=inch,
-                                    bottomMargin=inch/2, 
+            pdfDocument = SimpleDocTemplate(buffer, 
+                                    rightMargin=inch/3,
+                                    leftMargin=inch/3, 
+                                    topMargin=inch * 1.5,
+                                    bottomMargin=inch * 1, 
                                     pagesize=self.pagesize)
-            elements = []
+            pdfElements = []
             styles = getSampleStyleSheet()
             styles.add(ParagraphStyle(name='centered', alignment=TA_CENTER))
 
-            elements.append(Paragraph('My questions are', styles['Heading1']))
+            space = ' &nbsp;' * 17
+
+            headerText = "<b>Name: </b> %s %s <b>Facility Name: </b> %s" % (hData['name'], space, hData['facility'])
+            pdfElements.append(Paragraph(headerText, styles['Normal']))
+            pdfElements.append(Spacer(0, 3))
+            dateText = "<b>Survey Date:</b> %s" % hData['date']
+            pdfElements.append(Paragraph(dateText, styles['Normal']))
+            pdfElements.append(Spacer(0, 5))
+            # Draw a line under the headers
+            pdfElements.append(HRFlowable(width="100%", color=colors.black))
 
             for key, value in data.items():
-                elements.append(Paragraph(str(key) + '. Question:', styles['Heading5']))
-                elements.append(Paragraph(value[0], styles['Normal']))
-                elements.append(Spacer(0, 7))
-                elements.append(Paragraph('Your Answer:', styles['Heading5']))
-                elements.append(Paragraph(value[1], styles['Normal']))
-                elements.append(Spacer(0, 7))
-                elements.append(Paragraph('Recommendation:', styles['Heading5']))
-                elements.append(Paragraph(value[2], styles['Normal']))
-                elements.append(Spacer(0, 5))
-                elements.append(HRFlowable(width="100%", thickness=1, lineCap='round', spaceBefore=1, spaceAfter=1, hAlign='CENTER', vAlign='BOTTOM', dash=None))
+                pdfElements.append(Spacer(0, 5))
+                pdfElements.append(Paragraph('<b>' + str(key) + '. Question:</b>', styles['Normal']))
+                pdfElements.append(Paragraph(value[0], styles['Normal']))
+                pdfElements.append(Spacer(0, 5))
+                pdfElements.append(Paragraph('<b>Your Answer:</b>', styles['Normal']))
+                pdfElements.append(Paragraph(value[1], styles['Normal']))
+                pdfElements.append(Spacer(0, 5))
+                pdfElements.append(Paragraph('<b>Recommendation:</b>', styles['Normal']))
+                pdfElements.append(Paragraph(value[2], styles['Normal']))
+                pdfElements.append(Spacer(0, 5))
+                pdfElements.append(HRFlowable(width="100%", color=colors.black))
 
-            doc.build(elements, onFirstPage=self._header_footer, onLaterPages=self._header_footer)
+            pdfDocument.build(pdfElements, 
+                            onFirstPage=self._header_footer, 
+                            onLaterPages=self._header_footer)
         except:
             return False
         else:
